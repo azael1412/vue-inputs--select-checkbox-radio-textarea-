5750d9e965e32d6c58a4bcc747037562d3801c4d
azael1412 <azael_1412@hotmail.com>
2020-07-24 17:14:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+new Vue({
+  el: '#app',
+  data:{
+    estudios: 'Sin estudios',
+    terminos: false,
+     lenguajes: [],
+     dia:'',
+     comentarios:''
+  }
+  // template: '<h1>hello Vue</h1>'
+})
